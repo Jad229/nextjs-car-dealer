6319c0d80f43410f304f6e4b0414ca170ca72d61
Jad229 <76543236+Jad229@users.noreply.github.com>
2023-03-27 18:34:31 -0400

    FEATURE: navbar with simple hamburger menu

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Head from "next/head";
+import { useState } from "react";
 
 function HomePage() {
+  const [isNavOpen, setIsNavOpen] = useState(false); //initial hamburger menu state is set to false
+
   return (
     <div>
       <Head>
@@ -12,10 +17,21 @@ function HomePage() {
             <span className="text-2xl text-amber-400 font-semibold">
               Ideal Car Corp
             </span>
-            <div className="flex space-x-4 text-amber-400">
+            <div
+              id="navbar-links"
+              className={`${
+                isNavOpen ? "flex" : "hidden"
+              } md:flex space-x-4 text-amber-400`}
+              style={{ transition: "height 0.3s ease-in-out" }}
+            >
               <a href="#">Garage</a>
               <a href="#">About</a>
               <a href="#">Contact</a>
+            </div>
+            <div className="md:hidden">
+              <button onClick={() => setIsNavOpen(!isNavOpen)}>
+                <FontAwesomeIcon icon={faBars} style={{ color: "#ffffff" }} />
+              </button>
             </div>
           </div>
         </div>
