850e744fa24e63166c7895306c34354edd58e5ed
Jad229 <76543236+Jad229@users.noreply.github.com>
2023-03-27 16:41:22 -0400

    FIX: edited config files for css

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
+import Head from "next/head";
+
 function HomePage() {
-  return <div>Welcome to Next.js!</div>;
+  return (
+    <div>
+      <Head>
+        <title>Ideal Car Corp | Home</title>
+      </Head>
+    </div>
+  );
 }
 
 export default HomePage;
