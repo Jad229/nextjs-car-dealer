9d21a1830e5d3e8e5aeae54c0bdfebf0d7d082cf
Jad229 <76543236+Jad229@users.noreply.github.com>
2023-03-28 14:11:09 -0400

    FIX: started mobile homepage

--- pages/index.js
+++ pages/index.js
@@ -8,9 +8,26 @@ function HomePage() {
         <title>Ideal Car Corp | Home</title>
       </Head>
       <Navbar />
-      <div className="mx-auto w-full object-cover md:w-5/6">
-        <img className="md:rounded-2xl" src="/images/porsche-hero.png"></img>
-        <p>IDEAL CAR CORP</p>
+      <div className="relative mx-auto w-full object-cover md:w-5/6">
+        <img
+          className="invisible md:visible md:rounded-2xl"
+          src="/images/porsche-hero.png"
+        ></img>
+        <div className="absolute top-3 md:top-32 bottom-1/2 left-1/2 -translate-x-1/2">
+          <h1 className=" text-3xl text-amber-400">
+            Your Ideal Car is Waiting...
+          </h1>
+          <p className="text-xl">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A
+            asperiores beatae, error est ex explicabo.
+          </p>
+          <button
+            type="button"
+            className="button w-full bg-amber-400 rounded-xl p-2"
+          >
+            Get in Touch
+          </button>
+        </div>
       </div>
     </div>
   );
