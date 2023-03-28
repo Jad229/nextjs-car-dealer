395aa0dfd5f0024640f4945ebcf536c36c21fac7
Jad229 <76543236+Jad229@users.noreply.github.com>
2023-03-28 12:35:54 -0400

    FEATURE: added hero image

--- pages/index.js
+++ pages/index.js
@@ -7,11 +7,11 @@ function HomePage() {
   const [isNavOpen, setIsNavOpen] = useState(false); //initial hamburger menu state is set to false
 
   return (
-    <div>
+    <div className="w-screen h-screen">
       <Head>
         <title>Ideal Car Corp | Home</title>
       </Head>
-      <nav className="sticky top-0 z-10 bg-white backdrop-filter backdrop-blur-lg bg-opacity-30 border-b border-amber-400">
+      <nav className="sticky top-0 z-10 bg-white backdrop-filter backdrop-blur-lg bg-opacity-10 border-b border-amber-400">
         <div className="max-w-5xl mx-auto px-4">
           <div className="flex items-center justify-between h-16">
             <span className="text-2xl text-amber-400 font-semibold">
@@ -36,6 +36,10 @@ function HomePage() {
           </div>
         </div>
       </nav>
+      <div className="mx-auto w-full object-cover md:w-5/6">
+        <img className="md:rounded-2xl" src="/images/porsche-hero.png"></img>
+        <p>IDEAL CAR CORP</p>
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,11 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "hero-image": "url('/images/porsche-hero.png')",
+      },
+    },
   },
   plugins: [],
 };
