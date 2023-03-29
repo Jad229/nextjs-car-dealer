12c41136c3694fb01b1eb3e35fb23113d3a32433
Jad229 <76543236+Jad229@users.noreply.github.com>
2023-03-29 14:17:46 -0400

    CHANGES: rebuilt home hero

--- app/page.js
+++ app/page.js
@@ -11,10 +11,12 @@ function HomePage() {
       <Navbar />
       <div className="shadow-2xl md:relative mx-auto w-full md:w-5/6">
         <img className="md:rounded-2xl" src="/images/porsche-hero.png"></img>
-        <div className="rounded-b-2xl bg-slate-400 md:absolute md:top-32 md:bottom-1/2 md:left-1/2 md:-translate-x-1/2">
-          <h1 className="flex justify-center text-2xl text-amber-400">
-            Your Ideal Car is Waiting...
-          </h1>
+        <div className="rounded-b-2xl md:absolute md:top-32 md:bottom-1/2 md:left-1/2 md:-translate-x-1/2">
+          <div className="flex flex-col text-center text-4xl font-bold text-amber-400">
+            <span>CARS.</span>
+            <span>IDEAL.</span>
+            <span>HERE.</span>
+          </div>
           <p className="hidden md:block text-lg text-white">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. A
             asperiores beatae, error est ex explicabo.
