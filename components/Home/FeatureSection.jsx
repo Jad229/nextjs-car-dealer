827c7c678e8e752f86136cd7085c3d346e9f7a10
Jad229 <jad229@njit.edu>
2023-04-01 22:23:05 -0400

    FEATURE: featured sections started

--- components/Home/FeatureSection.jsx
+++ components/Home/FeatureSection.jsx
@@ -2,8 +2,10 @@ import React from "react";
 
 export default function FeaturedCard() {
   return (
-    <div>
-      <div>Feature Card</div>
-    </div>
+    <section id="feature-section" className="w-full">
+      <div>
+        <div>Featured Card</div>
+      </div>
+    </section>
   );
 }
